895234149bd997b5d6cf524d6ce93148fc1c3052
jeann9ne <jeanninecoons@gmail.com>
2020-05-14 21:19:42 -0400

    step two

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <div class="navbar">
+      <ul>
+         <li>Today</li>
+        <li>Calendar
+          <ul>
+             <li>Weekly</li>
+             <li>Monthly</li>
+          </ul>
+        </li>
+        <li>Goals</li>
+        <li>Archive</li>
+        <li>Settings</li>
+      </ul>
+       <input type="text" placeholder="Search"></input>
+     </div>
+
+     <div class="to-do-list">
+       <h1>Today</h1>
+       <form action = ''>
+         <fieldset>
+          <input type = "checkbox"
+              id = "chkDone"
+              value = "thingOne" />
+              <p>Just an example</p>
+          <input type = "checkbox"
+              id = "chkDone"
+              value = "thingTwo" />
+              <p>Just an example</p>
+         </fieldset>
+       </form>
+     </div>
     </div>
   );
 }
