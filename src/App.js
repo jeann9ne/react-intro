45a2502bccef8ea864c128e8169fcc4f7484f1e5
jeann9ne <jeanninecoons@gmail.com>
2020-05-19 18:19:15 -0400

    assignment complete

--- src/App.js
+++ src/App.js
@@ -1,41 +1,67 @@
 import React from 'react';
 import './App.css';
+import userImage from './userIcon.png';
 
+const toDoList= [{
+  id: 1,
+  title: "gotta do this",
+  description: "what exactly needs to get done.",
+  completed: false
+  },
+  {
+   id: 2,
+   title: "better do that",
+   description: "another thing that needs done",
+   completed: false
+  },
+  {
+    id: 3, 
+    title: "don't forget this",
+    description: "the thing I need to remember",
+    completed: false
+  },
+  {
+    id: 4,
+    title: "gotta do this",
+    description: "this is really important",
+    completed: false
+  }]
+
+  const userInfo= {
+    name: "Morpheus",
+    avatar: userImage
+  }
 
 function App() {
   return (
-    <div className="App">
+    <div class="body">
   
     <div class="navbar">
-      <ul>
-         <li>Today</li>
-        <li>Calendar
-          <ul>
-             <li>Weekly</li>
-             <li>Monthly</li>
+      <img class="user-img" src={userInfo.avatar} alt="user"></img>
+      <p>{userInfo.name}</p>
+      <ul class="navlist">
+         <li> <i class="fas fa-home"></i> Today</li>
+        <li> <i class="fas fa-calendar"></i> Calendar
+          <ul class="navlist">
+             <li> <i class="fas fa-calendar-week"></i> Weekly</li>
+             <li> <i class="fas fa-calendar-alt"></i> Monthly</li>
           </ul>
         </li>
-        <li>Goals</li>
-        <li>Archive</li>
-        <li>Settings</li>
+        <li> <i class="fas fa-bullseye"></i> Goals</li>
+        <li> <i class="fas fa-archive"></i> Archive</li>
+        <li> <i class="fas fa-cog"></i> Settings</li>
       </ul>
-       <input type="text" placeholder="Search"></input>
+       <input class="search-bar" type="text" placeholder="Search"></input>
      </div>
 
      <div class="to-do-list">
        <h1>Today</h1>
-       <form action = ''>
-         <fieldset>
-          <input type = "checkbox"
+              <ul class="navlist">
+                {toDoList.map (toDoItem => (<li> <input 
+              type = "checkbox"
               id = "chkDone"
-              value = "thingOne" />
-              <p>Just an example</p>
-          <input type = "checkbox"
-              id = "chkDone"
-              value = "thingTwo" />
-              <p>Just an example</p>
-         </fieldset>
-       </form>
+              value = "thingOne" /> 
+              {toDoItem.title} </li>))}</ul>
      </div>
     </div>
   );
